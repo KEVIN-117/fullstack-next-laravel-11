26a3156645806c180a80853694bf984780b3c498
Kevin Alexis Roodriguez Condori <krodriguezcondori34@gmai.com>
2024-08-23 05:39:31 -0400

    migrate product images

--- server/database/seeders/ProductTableSeeder.php
+++ server/database/seeders/ProductTableSeeder.php
@@ -21,7 +21,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'a01288eb-9a9c-45a2-976b-f314c4702006', // Electronics
                 'stock' => 150,
                 'price' => 699.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723181033/movile-bg/d46d1d6b99add90e6206384de47c7c18_peajv8.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401779/inventory-ecomerce/products/ocnmnwcypkndin8cuycb.jpg',
             ],
             [
                 'name' => 'Laptop',
@@ -30,7 +30,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'a01288eb-9a9c-45a2-976b-f314c4702006', // Electronics
                 'stock' => 50,
                 'price' => 1099.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723181032/movile-bg/9540fbbebdce5a97b25d25cfea2bc0a6_krmuyy.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401723/inventory-ecomerce/products/o0y64v5njidggaroex6x.jpg',
             ],
             [
                 'name' => 'Refrigerator',
@@ -39,7 +39,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'd143e3f2-c9d7-4eec-a5af-eb1b70f000a6', // Home Appliances
                 'stock' => 20,
                 'price' => 899.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723180896/movile-bg/d46d1d6b99add90e6206384de47c7c18_ojjuaa.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401662/inventory-ecomerce/products/zv47f0xuu8tkbu5v7jyl.jpg',
             ],
             [
                 'name' => 'Washing Machine',
@@ -48,7 +48,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'd143e3f2-c9d7-4eec-a5af-eb1b70f000a6', // Home Appliances
                 'stock' => 30,
                 'price' => 499.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140812/movile-bg/fc6705723e362220d554f62de472cefb_movktx.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401632/inventory-ecomerce/products/zl6nhbxlbr9jymlxeuwj.jpg',
             ],
             [
                 'name' => 'Fiction Novel',
@@ -57,7 +57,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'b433669b-a2b1-4893-864d-0a9ba187d9eb', // Books
                 'stock' => 200,
                 'price' => 19.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140812/movile-bg/ef95e0261d94344cd7cf59567e3f1326_z035hk.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401600/inventory-ecomerce/products/hauqlsmbo9x912peqeju.jpg',
             ],
             [
                 'name' => 'Textbook',
@@ -66,7 +66,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'b433669b-a2b1-4893-864d-0a9ba187d9eb', // Books
                 'stock' => 100,
                 'price' => 89.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140811/movile-bg/b7d65fe0f772bd587bad14f8ef2db786_a5lylh.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401489/inventory-ecomerce/products/fuqo1qcxthih4f6jky1b.jpg',
             ],
             [
                 'name' => 'Men\'s Jacket',
@@ -75,7 +75,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '3403a971-471e-448f-8c4d-fd7318bb8a15', // Fashion
                 'stock' => 75,
                 'price' => 129.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140810/movile-bg/b7288b101857d765089512e99422262b_kahy9h.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401397/inventory-ecomerce/products/llhsrd5iqe1zahjbvrjm.jpg',
             ],
             [
                 'name' => 'Women\'s Shoes',
@@ -84,7 +84,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '3403a971-471e-448f-8c4d-fd7318bb8a15', // Fashion
                 'stock' => 100,
                 'price' => 89.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140809/movile-bg/ac793ce8d40688c9882c51f8fab65968_ejhuxq.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401374/inventory-ecomerce/products/yqcxl5brqzvx4tl6noyg.jpg',
             ],
             [
                 'name' => 'Treadmill',
@@ -93,7 +93,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '575700f7-21cb-4a5e-ad17-622bbb7b3e24', // Sports & Outdoors
                 'stock' => 25,
                 'price' => 799.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140809/movile-bg/a3b8488391185f514fd984f850b166ec_fdqpu0.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401312/inventory-ecomerce/products/xnfx9qrsoopolldhgjft.jpg',
             ],
             [
                 'name' => 'Yoga Mat',
@@ -102,7 +102,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '575700f7-21cb-4a5e-ad17-622bbb7b3e24', // Sports & Outdoors
                 'stock' => 150,
                 'price' => 29.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140808/movile-bg/9530bbb4e4fdc7dd3530d1c0165afdf8_djyibb.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401344/inventory-ecomerce/products/bvjnsptqrv4fsrvocvci.jpg',
             ],
             [
                 'name' => 'Facial Cleanser',
@@ -111,7 +111,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '198896cd-7e3d-4486-9f62-5e068bfebf19', // Health & Beauty
                 'stock' => 200,
                 'price' => 14.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140807/movile-bg/93bd1d57ce5f5758124999b047a9f6dd_swtirj.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401289/inventory-ecomerce/products/oyzrjdj6c6dntedlvtmt.jpg',
             ],
             [
                 'name' => 'Shampoo',
@@ -120,7 +120,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '198896cd-7e3d-4486-9f62-5e068bfebf19', // Health & Beauty
                 'stock' => 180,
                 'price' => 9.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140806/movile-bg/8b7c3365ea02008105da2e15e737dc1d_b0jyln.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401265/inventory-ecomerce/products/dcf4ppgvl0sm5rcp7kme.jpg',
             ],
             [
                 'name' => 'Action Figure',
@@ -129,7 +129,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '5eea9366-8329-4519-857f-f2db3397ccb4', // Toys & Games
                 'stock' => 120,
                 'price' => 24.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140805/movile-bg/8af32198cf55af7c696f0d282a7cfab3_ooat1w.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401245/inventory-ecomerce/products/crbwb0qqafmoqk4g5rac.jpg',
             ],
             [
                 'name' => 'Puzzle',
@@ -138,7 +138,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '5eea9366-8329-4519-857f-f2db3397ccb4', // Toys & Games
                 'stock' => 90,
                 'price' => 14.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140804/movile-bg/679bda8a019f0ea156ea15545f4c1727_oimnpi.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401217/inventory-ecomerce/products/y7sw4bimkavet3lxolyc.jpg',
             ],
             [
                 'name' => 'Car Tires',
@@ -147,7 +147,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'eed2b6fb-0e1f-4e36-b7ff-8f038ab0010e', // Automotive
                 'stock' => 50,
                 'price' => 129.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140804/movile-bg/60d3fa1190e3d2e99521f4ba52de0cba_hefcnv.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401177/inventory-ecomerce/products/ug6adjrgdp7ftipa3sqp.jpg',
             ],
             [
                 'name' => 'Car Battery',
@@ -156,7 +156,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'eed2b6fb-0e1f-4e36-b7ff-8f038ab0010e', // Automotive
                 'stock' => 40,
                 'price' => 149.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140803/movile-bg/5b63c4d639e2b128112b5a8b41f17223_k3bk5c.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401149/inventory-ecomerce/products/b7bzg8iwjyyetzqs2eau.jpg',
             ],
             [
                 'name' => 'Office Chair',
@@ -165,7 +165,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '2a2d2000-1cb4-48e6-ad0d-e3c7a17c38ea', // Furniture
                 'stock' => 60,
                 'price' => 199.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140802/movile-bg/57bae4459ef41658af58adfa29840ac8_oe1olg.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401126/inventory-ecomerce/products/v1meape3nf8hexdve8lb.jpg',
             ],
             [
                 'name' => 'Dining Table',
@@ -174,7 +174,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '2a2d2000-1cb4-48e6-ad0d-e3c7a17c38ea', // Furniture
                 'stock' => 20,
                 'price' => 299.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140801/movile-bg/4534fdeff4204d648e286120efb1ea26_ix4pah.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401103/inventory-ecomerce/products/qfuxg6pdc0efqozfqdoz.jpg',
             ],
             [
                 'name' => 'Organic Coffee Beans',
@@ -183,7 +183,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '0017e7c8-dc72-4377-9357-12869c8e5944', // Grocery & Gourmet Food
                 'stock' => 100,
                 'price' => 19.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140801/movile-bg/3ef92f7757168fa6cc22b7f502e4150a_rbybu9.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724401075/inventory-ecomerce/products/tdtiemhnppcerxl0jcmx.jpg',
             ],
             [
                 'name' => 'Gourmet Chocolate',
@@ -192,7 +192,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => '0017e7c8-dc72-4377-9357-12869c8e5944', // Grocery & Gourmet Food
                 'stock' => 150,
                 'price' => 12.99,
-                'image' => 'https://res.cloudinary.com/dhq9acwqr/image/upload/v1723140799/movile-bg/34b55a5bf1fcbb3c78195acca38e0882_kygb6d.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724400974/inventory-ecomerce/products/v31epknihpo6auyraecz.jpg',
             ],
             [
                 'name' => 'Drone Camera',
@@ -201,7 +201,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'a01288eb-9a9c-45a2-976b-f314c4702006', // Grocery & Gourmet Food
                 'stock' => 10,
                 'price' => 299.99,
-                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1723685186/inventory-ecomerce/products/file_f6lrua.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724400935/inventory-ecomerce/products/ily5j8mdablsu0ecpai0.jpg',
             ],
             [
                 'name' => 'Gaming Laptop',
@@ -210,7 +210,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'a01288eb-9a9c-45a2-976b-f314c4702006',
                 'stock' => 20,
                 'price' => 1199.99,
-                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1723685611/inventory-ecomerce/products/file_vfimco.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724400894/inventory-ecomerce/products/dhqjy9msnd9r8xi6hyye.jpg',
             ],
             [
                 'name' => '4K Ultra HD TV',
@@ -219,7 +219,7 @@ class ProductTableSeeder extends Seeder
                 'category_id' => 'a01288eb-9a9c-45a2-976b-f314c4702006',
                 'stock' => 15,
                 'price' => 499.99,
-                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1723685668/inventory-ecomerce/products/file_vaeizg.jpg',
+                'image' => 'https://res.cloudinary.com/disvwilxi/image/upload/v1724400837/inventory-ecomerce/products/cllcbrrmcr9qb2sfc9gj.jpg',
             ],
         ];
 
