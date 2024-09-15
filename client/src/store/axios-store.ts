534837eaa78013d9cc15e408879e00f82bc114bd
Kevin Alexis Roodriguez Condori <krodriguezcondori34@gmai.com>
2024-09-15 02:56:12 -0400

    set up deploy config server and client

--- client/src/store/axios-store.ts
+++ client/src/store/axios-store.ts
@@ -1,13 +1,9 @@
 "use server";
 import axios from "axios";
-import { loadCookie, } from "@/utils/cookiesLoader"
 
-
-
-const token = loadCookie("INV_NEXT_TOKEN");
 
 const axiosInstance = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 })
 
 export default axiosInstance;
